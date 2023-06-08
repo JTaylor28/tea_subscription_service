8de183bd70f6564c7e6027422b6aa8139abeb564
James R Taylor <117054959+JTaylor28@users.noreply.github.com>
2023-06-08 15:15:40 -0600

    Refactor endpoint `POST` (#11)
    * Refactor: Post test to match new route
    
    * Refactor: Post action to be nested under customers subscriptions
    
    * Refactor: customer_id retrieved in params and not subscription_params
    
    * Delete: duplicate sub_request_spec
    
    * Fix: test typo

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,9 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :customers do
-        resources :subscriptions, only: [:index]
+        resources :subscriptions, only: [:index, :create]
       end
-      resources :subscriptions, only: [:update, :create]
+      resources :subscriptions, only: [:update]
     end
   end
 end
--- spec/requests/api/v1/subscriptions_request_spec.rb
+++ spec/requests/api/v1/subscriptions_request_spec.rb
@@ -1,20 +1,19 @@
 require "rails_helper"
 
 RSpec.describe "subscriptions", type: :request do
-  describe "POST /api/v1/subscriptions" do
+  describe "POST /api/v1/customers/customer_id/subscriptions" do
     it " creates a new subscription when given valid paramaters" do
       customer = Customer.create(first_name: "Bob", last_name: "Jones" , email: "bob@bobmail.com", address: "123 Bob St. Bob, CO 80302").id
       tea = Tea.create(title: "Green Tea", description: "Green Tea is green", temperature: 180, brew_time: 3, price: 10).id
 
       subscription_params = {
-          customer_id: customer,
           tea_id: tea,
           frequency: "monthly"
         }
 
       headers = {"CONTENT_TYPE" => "application/json", 'accept' => 'application/json'}
 
-      post "/api/v1/subscriptions", headers: headers, params: JSON.generate(subscription_params)
+      post "/api/v1/customers/#{customer}/subscriptions", headers: headers, params: JSON.generate(subscription_params)
       expect(response).to be_successful
       expect(Subscription.all.count).to eq(1)
       
