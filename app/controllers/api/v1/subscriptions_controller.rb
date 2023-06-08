047be7e4142004e3b7386429be86ecd97dabb294
James Taylor <jrtaylor0614@gmail.com>
2023-06-08 11:16:54 -0600

    Feat: Update to sub controller

--- app/controllers/api/v1/subscriptions_controller.rb
+++ app/controllers/api/v1/subscriptions_controller.rb
@@ -1,7 +1,18 @@
 class Api::V1::SubscriptionsController < ApplicationController
-
   def create
-    subscription = Subscription.make_sub(params[:customer_id], params[:tea_id], params[:frequency])
+    subscription = Subscription.make_sub(subscription_params[:customer_id], subscription_params[:tea_id], subscription_params[:frequency])
     render json: SubscriptionSerializer.new(subscription)
+  end
+
+  def update
+    subscription = Subscription.find(params[:id])
+    subscription.update(status: params[:status])
+    render json: SubscriptionSerializer.new(subscription)
+  end
+
+  private
+
+  def subscription_params
+    params.require(:subscription).permit(:customer_id, :tea_id, :frequency)
   end
 end
